4a9223b7c3f42d42fcf6b7048154e154f24fb8e8
ehxhfl727@gmail.com <>
2023-08-13 02:12:59 +0900

    SQL Dev Github 커밋

--- test2.sql
+++ test2.sql
@@ -6,3 +6,13 @@ CREATE TABLE Department (
     deptname VARCHAR2(40),
     manager VARCHAR2(40)
 );
+CREATE TABLE Employee(
+empno NUMBER PRIMARY KEY,
+deptno NUMBER ,
+name VARCHAR2(40),
+phoneno NUMBER,
+address VARCHAR2(40),
+gender VARCHAR2(40),
+posision VARCHAR2(40),
+foreign key(deptno) REFERENCES Department(deptno)
+);
