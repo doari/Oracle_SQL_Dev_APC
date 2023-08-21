3390391cf6a4c2a889f9cf8d8111ac98096fef73
민병준 <ehxhfl727@gmail.com>
2023-08-21 22:45:19 +0900

    데이터 조작 연습01-Data Manipulation Practice01

---
+++ select_01.sql
@@ -0,0 +1,45 @@
+-- book테이블에서 가장 비싼 가격을 보이시오
+select MAX(price)
+from book;
+
+select * from customer;
+select * from orders;
+-- book테이블에서 가장 비싼 가격의 도서이름을 보이시오.
+
+SELECT bookname
+FROM book
+WHERE price = (SELECT MAX(price) FROM book);
+
+-- IN
+SELECT bookname
+FROM book
+WHERE price IN (SELECT MAX(price) FROM book);
+
+-- 도서를 구매한 적이 있는 고객의 이름을 보이시오
+select custid,name
+from customer
+where custid IN (select custid from orders);
+
+-- 굿스포츠에서 출판한 도서를 구매한 적이 있는 고객의 이름을 보이시오
+select * from book;
+
+select name
+from customer
+where custid IN (select custid 
+                from orders
+                where bookid IN (select bookid
+                                 from book
+                                 where publisher='굿스포츠'));
+                                 
+                                 
+                                 
+-- EXISTS (null값 자동처리, 찾고자하는 값을 찾으면 자동으로 다음 행들을  무시하고 중지함으로 빠름)
+-- 주문한 적이 있는 고객의 이름과 주소를 보이시오
+select * from orders od, customer cs
+where cs.custid=od.custid;
+
+select name, address
+from customer cs
+where EXISTS(select * 
+            from orders od
+            where cs.custid=od.custid);
