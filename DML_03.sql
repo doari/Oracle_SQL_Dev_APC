da4602aca83de0a61ea7a643eeb15449de119ca2
민병준 <ehxhfl727@gmail.com>
2023-08-21 22:44:40 +0900

    데이터 조작의 기본3-Basics of Data Manipulation3

--- DML_03.sql
+++ DML_03.sql
@@ -61,3 +61,73 @@ delete from imported_book;
 select *
 from book
 order by price DESC, publisher ASC;
+
+-- 집계함수
+select Sum(saleprice) as 총매출
+from orders;
+
+-- 고객이 주문한 도서의 총 판매액, 최대값, 최솟값을 구하시오.
+SELECT
+    SUM(saleprice) as Total,
+    AVG(saleprice) as Average,
+    MAX(saleprice) as Minumun,
+    MIN(saleprice) as Maximun
+FROM orders;
+-- orders의 모든행 추출
+select * from orders;
+-- 튜플의 갯수세기 
+select count(orderid)
+from orders;
+
+-- 전체 튜플의 갯수
+select count(*)
+from orders;
+
+-- 도서 판매가격이 20000원 이상인 건수를 보이시오
+-- where 문
+SELECT
+    COUNT(*) AS 판매가격_20000원_이상인_건수
+FROM
+    orders
+WHERE
+    saleprice >= 20000;
+    
+-- case when then 사용
+SELECT count(
+(case when saleprice >= 20000 then 1 end)) as VIP
+FROM orders;
+
+-- 도서 판매가격이 20000원 이상인 건수와 10000이하의 권수를 구하시오
+SELECT count((case when saleprice >= 20000 then 1 end)) as VIP,
+       count((case when saleprice < 10000 then 1 end)) as cs
+FROM orders;
+
+-- GROUP BY
+-- 고객별로 주문한 도서의 총 수량과 총 판매액을 보이시오
+select custid, count(*) 도서수량, SUM(saleprice) as "총 판매액"
+from orders
+group by custid
+order by custid; --순서대로 정렬
+
+-- 가격이 15000원 이상인 도서를 구해한 고객에 대하여 고객별 주문도서의 총 수량를 보이시오
+select custid,count(case when saleprice >= 15000 then 1 end) as "15000원 이상인 도서"
+from orders
+group by custid
+order by custid; --순서대로 정렬
+
+-- 가격이 15000원 이상인 도서를 구해한 고객에 대하여 고객별 주문도서의 총 수량를 보이시오
+-- (단 2권이상 구매한 고객만 출력)-- 그룹안의 조건은 having
+select custid,count(case when saleprice >= 15000 then 1 end) as "15000원 이상인 도서"
+from orders
+where saleprice >=15000
+group by custid
+having count(*)>=2 -- 그룹안의 조건은 having
+order by custid; --순서대로 정렬
+-- 가격이 15000원 이상인 도서를 구해한 고객에 대하여 고객별 주문도서의 총 수량를 보이시오
+-- (단 2권이상 구매한 고객만 출력)-- 그룹안의 조건은 having
+select custid,count(*) as "15000원 이상인 도서"
+from orders
+where saleprice >=15000 --1
+group by custid
+having count(*)>=2
+order by custid; --2
